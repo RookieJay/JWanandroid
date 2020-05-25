fbb4dfe99308a0c9e487b6b2746f7b8a571ccb2c
RookieJay <1475485144@qq.com>
2020-05-25 11:46:40 +0800

    fix:NoSuchMethodError with getColor method on device smaller than API 23.（bugly:#11006 #11004）

--- app/src/main/java/pers/jay/wanandroid/utils/UIUtils.java
+++ app/src/main/java/pers/jay/wanandroid/utils/UIUtils.java
@@ -12,6 +12,7 @@ import android.renderscript.Allocation;
 import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
+import android.support.annotation.ColorRes;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.TypedValue;
@@ -26,6 +27,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import pers.jay.wanandroid.R;
+import pers.jay.wanandroid.common.JApplication;
 import timber.log.Timber;
 
 public class UIUtils {
@@ -34,7 +36,8 @@ public class UIUtils {
                                                   boolean cancelable,
                                                   DialogInterface.OnClickListener positiveListener,
                                                   DialogInterface.OnClickListener negativeListener) {
-        return new AlertDialog.Builder(context).setTitle(context.getResources().getString(R.string.title_warm_tips)) //标题
+        return new AlertDialog.Builder(context).setTitle(
+                context.getResources().getString(R.string.title_warm_tips)) //标题
                                                .setMessage(message)//内容
                                                //                .setIcon(R.drawable.bg_image_placeholder)//图标
                                                .setCancelable(cancelable) //可否取消
@@ -57,7 +60,7 @@ public class UIUtils {
      * dp转px
      */
     public static int dp2px(Context context, float dpVal) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpVal,
+        return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpVal,
                 context.getResources().getDisplayMetrics());
     }
 
@@ -70,8 +73,8 @@ public class UIUtils {
             window.clearFlags(
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
             window.getDecorView()
-                    .setSystemUiVisibility(
-                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                  .setSystemUiVisibility(
+                          View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
             window.setNavigationBarColor(Color.TRANSPARENT);
@@ -106,9 +109,10 @@ public class UIUtils {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             //设置状态栏颜色
             if (isLight) {
-                window.setStatusBarColor(resources.getColor(R.color.colorPrimary, null));
-            } else {
-                window.setStatusBarColor(resources.getColor(R.color.colorAccent, null));
+                window.setStatusBarColor(getColor(R.color.colorPrimary));
+            }
+            else {
+                window.setStatusBarColor(getColor(R.color.colorAccent));
             }
             //状态栏颜色接近于白色，文字图标变成黑色
             View decor = window.getDecorView();
@@ -116,7 +120,8 @@ public class UIUtils {
             if (isLight) {
                 //light --> a|=b的意思就是把a和b按位或然后赋值给a,   按位或的意思就是先把a和b都换成2进制，然后用或操作，相当于a=a|b
                 ui |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-            } else {
+            }
+            else {
                 //dark  --> &是位运算里面，与运算,  a&=b相当于 a = a&b,  ~非运算符
                 ui &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
             }
@@ -151,8 +156,8 @@ public class UIUtils {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                InputMethodManager inputMethodManager =
-                        (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                InputMethodManager inputMethodManager = (InputMethodManager)activity.getSystemService(
+                        Context.INPUT_METHOD_SERVICE);
                 inputMethodManager.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
             }
         }, 300);
@@ -164,18 +169,19 @@ public class UIUtils {
      * Note：缩小的系数应该为2的整数次幂 ，即上面代码中的scale应该为1/2、1/4、1/8 ...
      * 参考BitmapFactory.Options 对图片缩放 的inSample系数。据前辈们经验，一般scale = 1/8 为佳。
      */
-    public static Bitmap rsBlur(Context context, Bitmap source, int radius, float scale){
-        Timber.d("UiUtils "+"origin size:"+source.getWidth()+"*"+source.getHeight());
+    public static Bitmap rsBlur(Context context, Bitmap source, int radius, float scale) {
+        Timber.d("UiUtils " + "origin size:" + source.getWidth() + "*" + source.getHeight());
         int width = Math.round(source.getWidth() * scale);
         int height = Math.round(source.getHeight() * scale);
-        Bitmap inputBmp = Bitmap.createScaledBitmap(source,width,height,false);
-        RenderScript renderScript =  RenderScript.create(context);
-        Timber.i("UiUtils " +"scale size:"+inputBmp.getWidth()+"*"+inputBmp.getHeight());
+        Bitmap inputBmp = Bitmap.createScaledBitmap(source, width, height, false);
+        RenderScript renderScript = RenderScript.create(context);
+        Timber.i("UiUtils " + "scale size:" + inputBmp.getWidth() + "*" + inputBmp.getHeight());
         // Allocate memory for Renderscript to work with
-        final Allocation input = Allocation.createFromBitmap(renderScript,inputBmp);
-        final Allocation output = Allocation.createTyped(renderScript,input.getType());
+        final Allocation input = Allocation.createFromBitmap(renderScript, inputBmp);
+        final Allocation output = Allocation.createTyped(renderScript, input.getType());
         // Load up an instance of the specific script that we want to use.
-        ScriptIntrinsicBlur scriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript, Element.U8_4(renderScript));
+        ScriptIntrinsicBlur scriptIntrinsicBlur = ScriptIntrinsicBlur.create(renderScript,
+                Element.U8_4(renderScript));
         scriptIntrinsicBlur.setInput(input);
         // Set the blur radius
         // ** 设置模糊半径**：设置一个模糊的半径,其值为 0－25
@@ -186,5 +192,9 @@ public class UIUtils {
         output.copyTo(inputBmp);
         renderScript.destroy();
         return inputBmp;
+    }
+
+    public static int getColor(@ColorRes int color) {
+        return ContextCompat.getColor(JApplication.getInstance(), color);
     }
 }
